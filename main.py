546cbb70aca21123e9f21fba00027c1409910ad4
Kd-20028 <keshawndavis472@gmail.com>
2023-04-28 14:54:34 -0500

    Updated Functions (Using multiple tables)

--- main.py
+++ main.py
@@ -1,113 +1,97 @@
 import sqlite3
 
+# Function to view available t-shirts
+
 
 def view_tshirts():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('Tshirts-group.db')
+    conn = sqlite3.connect('ecommerce.db')
+    cursor = conn.cursor()
 
-    # Create a cursor object
-    cur = conn.cursor()
+    cursor.execute('SELECT * FROM t_shirts')
+    tshirts = cursor.fetchall()
 
-    # Execute a query to retrieve all rows from the t_shirts table
-    cur.execute('SELECT * FROM t_shirts')
-
-    # Fetch all the rows
-    rows = cur.fetchall()
-
-    # Print the rows
-    print("Here are all the t-shirts in the inventory:")
-    for row in rows:
+    print("Available t-shirts:")
+    for tshirt in tshirts:
         print(
-            f"Name: {row[1]}, Price: {row[2]}, Size: {row[3]}, Color: {row[4]}, Style: {row[5]}, Iventory: {row[6]}")
+            f"ID: {tshirt[0]}  Name: {tshirt[1]}  Color: {tshirt[2]}  Size: {tshirt[3]}  Quantity: {tshirt[4]}  Price: {tshirt[5]}")
 
-    # Close the cursor and database connection
-    cur.close()
     conn.close()
+
+# Function to add a t-shirt to the shopping cart
 
 
 def add_to_cart():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('Tshirts-group.db')
+    tshirt_id = int(
+        input("Enter the ID of the t-shirt you want to add to your cart: "))
+    quantity = int(input("Enter the quantity you want to add to your cart: "))
 
-    # Create a cursor object
-    cur = conn.cursor()
+    conn = sqlite3.connect('ecommerce.db')
+    cursor = conn.cursor()
 
-    # Execute a query to retrieve all rows from the t_shirts table
-    cur.execute('SELECT * FROM t_shirts')
+    cursor.execute(
+        'SELECT t_shirt_quantity FROM t_shirts WHERE t_shirt_id=?', (tshirt_id,))
+    tshirt_quantity = cursor.fetchone()[0]
 
-    # Fetch all the rows
-    rows = cur.fetchall()
-
-    # Print the rows with a numbered list for user selection
-    print("Select a shirt to add to your cart:")
-    for i, row in enumerate(rows):
-        print(
-            f"{i+1}. Name: {row[1]}, Price: {row[2]}, Size: {row[3]}, Color: {row[4]}, Style: {row[5]}, Inventory: {row[6]}")
-
-    # Get the user's choice
-    choice = int(
-        input("Enter the number of the shirt you want to add to your cart: "))
-
-    # Get the selected shirt's details
-    selected_shirt = rows[choice-1]
-
-    # Check if the inventory of the selected shirt is greater than 0
-    if selected_shirt[6] > 0:
-        # Decrement the inventory of the selected shirt by 1
-        cur.execute(
-            f"UPDATE t_shirts SET inventory = {selected_shirt[6]-1} WHERE id = {selected_shirt[0]}")
-        conn.commit()
-
-        # Display a message to confirm the shirt has been added to the cart
-        print(f"Added {selected_shirt[1]} to your cart! \n")
-
+    if tshirt_quantity < quantity:
+        print("Insufficient stock!")
     else:
-        # Display a message if the inventory of the selected shirt is 0
-        print("Sorry, this shirt is out of stock.")
+        cursor.execute(
+            'INSERT INTO shopping_cart (customer_id, t_shirt_id, quantity) VALUES (?, ?, ?)', (1, tshirt_id, quantity))
+        cursor.execute(
+            'UPDATE t_shirts SET t_shirt_quantity=t_shirt_quantity-? WHERE t_shirt_id=?', (quantity, tshirt_id))
+        conn.commit()
+        print("T-shirt added to cart successfully!")
 
-    # Close the cursor and database connection
-    cur.close()
     conn.close()
+
+# Function to remove a t-shirt from the shopping cart
 
 
 def remove_from_cart():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('Tshirts-group.db')
+    cart_id = int(
+        input("Enter the ID of the item you want to remove from your cart: "))
 
-    # Create a cursor object
-    cur = conn.cursor()
+    conn = sqlite3.connect('ecommerce.db')
+    cursor = conn.cursor()
 
-    # Execute a query to retrieve the last added shirt from the t_shirts table
-    cur.execute('SELECT * FROM t_shirts ORDER BY id DESC LIMIT 1')
+    cursor.execute('SELECT * FROM shopping_cart WHERE cart_id=?', (cart_id,))
+    item = cursor.fetchone()
 
-    # Fetch the last added shirt
-    last_added_shirt = cur.fetchone()
-
-    # Ask the user if they want to remove the last added shirt from their cart
-    user_input = input(
-        f"Do you want to remove {last_added_shirt[1]} from your cart? (y/n) \n")
-
-    if user_input.lower() == 'y':
-        # Increment the inventory of the last added shirt by 1
-        cur.execute(
-            f"UPDATE t_shirts SET inventory = {last_added_shirt[6]+1} WHERE id = {last_added_shirt[0]}")
-        conn.commit()
-
-        # Display a message to confirm the shirt has been removed from the cart
-        print(f"Removed {last_added_shirt[1]} from your cart.")
+    if item is None:
+        print("Item not found in cart!")
     else:
-        # Display a message that the shirt has not been removed from the cart
-        print(f"{last_added_shirt[1]} has not been removed from your cart.")
+        cursor.execute('DELETE FROM shopping_cart WHERE cart_id=?', (cart_id,))
+        cursor.execute(
+            'UPDATE t_shirts SET quantity=quantity+? WHERE t_shirt_id=?', (item[3], item[2]))
+        conn.commit()
+        print("Item removed from cart successfully!")
 
-    # Close the cursor and database connection
-    cur.close()
     conn.close()
 
+# Main function to handle user input and call appropriate functions
 
-user_input = input(
-    f"Would you like to view all the shirts in the inventory? (y/n) ")
-if user_input.lower() == 'y':
-    view_tshirts()
-    u_input = input(f"Would you like to add shirt to your cart? (y/n) ")
-    if u_input.lower() == 'y':
-        add_to_cart()
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. View available t-shirts")
+        print("2. Add a t-shirt to your cart")
+        print("3. Remove a t-shirt from your cart")
+        print("4. Quit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            view_tshirts()
+        elif choice == 2:
+            add_to_cart()
+        elif choice == 3:
+            remove_from_cart()
+        elif choice == 4:
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+if __name__ == '__main__':
+    main()
