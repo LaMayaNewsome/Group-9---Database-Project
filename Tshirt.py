c0751ea193129ad8cd973114cd1d084c73528ede
finalfantasynerd <harveyfoley2002@gmail.com>
2023-04-29 13:26:49 -0500

    Update Tshirt.py
    Removed add_to_cart and remove_to_cart functions

--- Tshirt.py
+++ Tshirt.py
@@ -17,59 +17,7 @@ def view_tshirts():
 
     conn.close()
 
-# Function to add a t-shirt to the shopping cart
-
-
-def add_to_cart():
-    tshirt_id = int(
-        input("Enter the ID of the t-shirt you want to add to your cart: "))
-    quantity = int(input("Enter the quantity you want to add to your cart: "))
-
-    conn = sqlite3.connect('site.db')
-    cursor = conn.cursor()
-
-    cursor.execute(
-        'SELECT t_shirt_quantity FROM t_shirts WHERE t_shirt_id=?', (tshirt_id,))
-    tshirt_quantity = cursor.fetchone()[0]
-
-    if tshirt_quantity < quantity:
-        print("Insufficient stock!")
-    else:
-        cursor.execute(
-            'INSERT INTO shopping_cart (customer_id, t_shirt_id, quantity) VALUES (?, ?, ?)', (1, tshirt_id, quantity))
-        cursor.execute(
-            'UPDATE t_shirts SET t_shirt_quantity=t_shirt_quantity-? WHERE t_shirt_id=?', (quantity, tshirt_id))
-        conn.commit()
-        print("T-shirt added to cart successfully!")
-
-    conn.close()
-
-# Function to remove a t-shirt from the shopping cart
-
-
-def remove_from_cart():
-    cart_id = int(
-        input("Enter the ID of the item you want to remove from your cart: "))
-
-    conn = sqlite3.connect('site.db')
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM shopping_cart WHERE cart_id=?', (cart_id,))
-    item = cursor.fetchone()
-
-    if item is None:
-        print("Item not found in cart!")
-    else:
-        cursor.execute('DELETE FROM shopping_cart WHERE cart_id=?', (cart_id,))
-        cursor.execute(
-            'UPDATE t_shirts SET quantity=quantity+? WHERE t_shirt_id=?', (item[3], item[2]))
-        conn.commit()
-        print("Item removed from cart successfully!")
-
-    conn.close()
-
 # Main function to handle user input and call appropriate functions
-
 
 def main():
     while True:
