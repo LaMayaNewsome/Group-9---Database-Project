f578607d186dff13b6d94f45fdda198266e18cef
finalfantasynerd <harveyfoley2002@gmail.com>
2023-04-28 21:37:57 -0500

    Update inventory_Functions.py

--- inventory_Functions.py
+++ inventory_Functions.py
@@ -14,40 +14,68 @@ def inventory_editQuantity(item_Quantity):
     # Close the database connection
     conn.close()
 
-def inventory_List():
-    # Connect to the SQLite database
+def add_to_cart():
+    item_id = int(input("Enter the ID of the product (either tshirt or video game) you want to add to your cart: "))
+    quantity = int(input("Enter the quantity you want to add to your cart: "))
+
     conn = sqlite3.connect('site.db')
-    c = conn.cursor()
+    cursor = conn.cursor()
 
-    # Execute an SQL UPDATE statement to update values in the database
-    c.execute("SELECT item_Name, item_Category, item_Quantity FROM inventory")
+    if item_id
+    cursor.execute('SELECT t_shirt_quantity FROM t_shirts WHERE t_shirt_id=?', (tshirt_id,))
+    tshirt_quantity = cursor.fetchone()[0]
 
-    rows = c.fetchall()
+    if tshirt_quantity < quantity:
+        print("Insufficient stock!")
+    else:
+        cursor.execute(
+            'INSERT INTO shopping_cart (customer_id, t_shirt_id, quantity) VALUES (?, ?, ?)', (1, tshirt_id, quantity))
+        cursor.execute(
+            'UPDATE t_shirts SET t_shirt_quantity=t_shirt_quantity-? WHERE t_shirt_id=?', (quantity, tshirt_id))
+        conn.commit()
+        print("T-shirt added to cart successfully!")
 
-    for row in rows:
-        item_Name, item_Category, item_Quantity = row
-        print(f"Item: {item_Name} - Category: {item_Category} - Quantity: {item_Quantity}")
-
-    # Close the database connection
     conn.close()
 
-    # Main function to handle user input and call appropriate functions
+def remove_from_cart():
+    cart_id = int(
+        input("Enter the ID of the item you want to remove from your cart: "))
+
+    conn = sqlite3.connect('site.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM shopping_cart WHERE cart_id=?', (cart_id,))
+    item = cursor.fetchone()
+
+    if item is None:
+        print("Item not found in cart!")
+    else:
+        cursor.execute('DELETE FROM shopping_cart WHERE cart_id=?', (cart_id,))
+        cursor.execute(
+            'UPDATE t_shirts SET quantity=quantity+? WHERE t_shirt_id=?', (item[3], item[2]))
+        conn.commit()
+        print("Item removed from cart successfully!")
+
+    conn.close()
 
 
 def main():
     while True:
         print("\nMenu:")
-        print("1. View available items")
-        print("2. Edit an item's quantity in inventory")
-        print("3. Quit")
+        print("1. Edit an item's Quantity")
+        print("2. Add Item to Shopping Cart")
+        print("3. Remove item from Shopping Cart")
+        print("4. Quit")
 
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            inventory_List()
-        elif choice == 2:
             inventory_editQuantity()
+        elif choice == 2:
+            add_to_cart()
         elif choice == 3:
+            remove_from_cart()
+        elif choice == 4:
             break
         else:
             print("Invalid choice, please try again.")
