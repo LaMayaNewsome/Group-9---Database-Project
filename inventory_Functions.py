e859c8c956360454d8f5cf861ff4d8ac7e5343ee
finalfantasynerd <128729159+finalfantasynerd@users.noreply.github.com>
2023-04-28 22:19:31 -0500

    Update inventory_Functions.py

--- inventory_Functions.py
+++ inventory_Functions.py
@@ -1,12 +1,20 @@
 import sqlite3
 
 def inventory_editQuantity(item_Quantity):
-    # Connect to the SQLite database
+    item_id = int(input("Enter the ID of the product (either tshirt or video game) you want change the quantity of: "))
+
     conn = sqlite3.connect('site.db')
     c = conn.cursor()
 
     # Execute an SQL UPDATE statement to update values in the database
-    c.execute("UPDATE inventory SET quantity = ?", (item_Quantity))
+    if item_id in range(101,103):
+        c.execute("UPDATE inventory SET quantity = ?", (item_Quantity))
+    
+    elif item_id in range(1,3):
+        c.execute("UPDATE inventory SET quantity = ?", (item_Quantity))
+    
+    else:
+        print("Item ID Not Found, Please Try Again...")
 
     # Commit the transaction
     conn.commit()
@@ -21,19 +29,37 @@ def add_to_cart():
     conn = sqlite3.connect('site.db')
     cursor = conn.cursor()
 
-    if item_id
-    cursor.execute('SELECT t_shirt_quantity FROM t_shirts WHERE t_shirt_id=?', (tshirt_id,))
-    tshirt_quantity = cursor.fetchone()[0]
+    if item_id in range(101,103): #T-Shirt Selection
+        cursor.execute('SELECT item_quantity FROM t_shirts WHERE t_shirt_id=?', (item_id))
+        item_quantity = cursor.fetchone()[0]
 
-    if tshirt_quantity < quantity:
-        print("Insufficient stock!")
+        if item_quantity < quantity:
+            print("Insufficient stock!")
+        else:
+            cursor.execute(
+                'INSERT INTO shopping_cart (customer_id, t_shirt_id, quantity) VALUES (?, ?, ?)', (1, item_id, item_quantity))
+            cursor.execute(
+                'UPDATE inventory SET item_quantity=item_quantity-? WHERE t_shirt_id=?', (quantity, item_id))
+            conn.commit()
+            print("T-shirt added to cart successfully!")
+    
+    elif item_id in range(1,3): #Video Game Selection
+        cursor.execute('SELECT item_quantity FROM t_shirts WHERE gameID=?', (item_id))
+        item_quantity = cursor.fetchone()[0]
+
+        if item_quantity < quantity:
+            print("Insufficient stock!")
+        else:
+            cursor.execute(
+                'INSERT INTO shopping_cart (customer_id, gameID, quantity) VALUES (?, ?, ?)', (1, item_id, quantity))
+            cursor.execute(
+                'UPDATE inventory SET item_quantity=item_quantity-? WHERE gameID=?', (quantity, item_id))
+            conn.commit()
+            print("Video Game added to cart successfully!")
+    
     else:
-        cursor.execute(
-            'INSERT INTO shopping_cart (customer_id, t_shirt_id, quantity) VALUES (?, ?, ?)', (1, tshirt_id, quantity))
-        cursor.execute(
-            'UPDATE t_shirts SET t_shirt_quantity=t_shirt_quantity-? WHERE t_shirt_id=?', (quantity, tshirt_id))
-        conn.commit()
-        print("T-shirt added to cart successfully!")
+        print("Product ID Not Found, Please Try Again! :D")
+    
 
     conn.close()
 
@@ -44,15 +70,21 @@ def remove_from_cart():
     conn = sqlite3.connect('site.db')
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM shopping_cart WHERE cart_id=?', (cart_id,))
+    cursor.execute('SELECT * FROM shopping_cart WHERE cart_id=?', (cart_id))
     item = cursor.fetchone()
 
     if item is None:
         print("Item not found in cart!")
-    else:
+    elif item in range(101,103):
         cursor.execute('DELETE FROM shopping_cart WHERE cart_id=?', (cart_id,))
         cursor.execute(
-            'UPDATE t_shirts SET quantity=quantity+? WHERE t_shirt_id=?', (item[3], item[2]))
+            'UPDATE inventory SET quantity=quantity+? WHERE t_shirt_id=?', (item[3], item[2]))
+        conn.commit()
+        print("Item removed from cart successfully!")
+    elif item in range(1,3):
+        cursor.execute('DELETE FROM shopping_cart WHERE cart_id=?', (cart_id,))
+        cursor.execute(
+            'UPDATE inventory SET quantity=quantity+? WHERE gameID=?', (item[3], item[2]))
         conn.commit()
         print("Item removed from cart successfully!")
 
