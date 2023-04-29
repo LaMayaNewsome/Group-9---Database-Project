4fbcd4803b56adea8ac6adc34532042436fe6788
SamHankins229 <samhankins@outlook.com>
2023-04-29 02:24:16 -0500

    Update DeleteUser.py

--- DeleteUser.py
+++ DeleteUser.py
@@ -2,9 +2,9 @@ import sqlite3
 
 #Get DataBases figured out namewise and such so you can delete ShipAd and PayInfo along with user.
 
-conn = sqlite3.connect('login.db')
+conn = sqlite3.connect('user.db')
 
-conn.execute("DELETE FROM login WHERE username = ?", ('current user',))
+conn.execute("DELETE FROM user WHERE username = ?, DELETE FROM user WHERE paymentinfo = ?, DELETE FROM user WHERE shippingaddress = ?", ('current user',))
 
 conn.commit()
 conn.close()
